bd3c8aff15eb69ac7ab40d131fddd3b71f4a4d94
liuji <liuji@cycfc.com>
2021-03-11 11:32:50 +0800

    add datas

---
+++ datas/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+
+# Create your models here.
+class History(models.Model):
+    id = models.AutoField(primary_key=True)
+    red1 = models.CharField(max_length=20, blank=True)
+    red2 = models.CharField(max_length=20, blank=True)
+    red3 = models.CharField(max_length=20, blank=True)
+    red4 = models.CharField(max_length=20, blank=True)
+    red5 = models.CharField(max_length=20, blank=True)
+    red6 = models.CharField(max_length=20, blank=True)
+    blue = models.CharField(max_length=20, blank=True)
+
+    class Meta:
+        db_table = 't_history'
